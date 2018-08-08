521af732437cec4ec32f78ae2bdbb0e6586e1cc9
MiLeung <michaeleleung@gmail.com>
2018-08-07 21:38:54 -0700

    example

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-import SpriteSheet from './src/SpriteSheet.js';
+import SpriteSheet from './src/SpriteSheet';
+
+import { AppRegistry } from 'react-native';
+import Example from './src/Example';
+import { name } from './app.json';
+AppRegistry.registerComponent(name, () => Example);
+
 export default SpriteSheet;
